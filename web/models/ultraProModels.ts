b8c20855c455cfe670cf84d1822562ee0a3d3d67
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-29 18:01:04 +0630

    Create ultraProModels.ts

---
+++ web/models/ultraProModels.ts
@@ -0,0 +1,23 @@
+export type UltraProExperienceItem = {
+  role: string;
+  company: string;
+  period: string;
+  description: string;
+};
+
+export type UltraProProjectItem = {
+  title: string;
+  description: string;
+  tags: string[];
+};
+
+export type UltraProTestimonialItem = {
+  quote: string;
+  author: string;
+  role: string;
+};
+
+export type UltraProSocialLink = {
+  label: string;
+  href: string;
+};
