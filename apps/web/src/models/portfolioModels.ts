779d83e04edce3b7f5d162ed74ad4106f20433d2
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-30 02:53:22 +0630

    Create portfolioModels.ts

---
+++ apps/web/src/models/portfolioModels.ts
@@ -0,0 +1,23 @@
+export type ExperienceItem = {
+  role: string;
+  company: string;
+  period: string;
+  description: string;
+};
+
+export type ProjectItem = {
+  title: string;
+  description: string;
+  tags: string[];
+};
+
+export type TestimonialItem = {
+  quote: string;
+  author: string;
+  role: string;
+};
+
+export type SocialLinkItem = {
+  label: string;
+  href: string;
+};
